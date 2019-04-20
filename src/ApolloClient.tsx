dca3354347d023febd7dd5b06438acc5ae0961eb
dafrie <dav.friede@gmail.com>
2019-04-20 20:22:51 +0200

    #2  basic views: CRUD chapters (#26)
    * "ejected" from apollo-boost for local state management
    
    * add currentRole to authClient
    
    * add local resolver & query for settings (for now)
    
    * Setting wizard + setting screen in sections
    
    * use formik for newChapter mutations
    
    * removed unecessary comment
    
    * add changes from other branch regarding codegen

--- src/ApolloClient.tsx
+++ src/ApolloClient.tsx
@@ -1,14 +1,95 @@
-import ApolloClient from "apollo-boost";
-import auth0Client from "./auth/Auth";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink, Observable } from "apollo-link";
 
-export default new ApolloClient({
-  uri: process.env.REACT_APP_BACKEND_URL,
-  // On each request, set the authorization header
-  request: async operation => {
-    operation.setContext({
-      headers: {
-        authorization: 'JWT ' + auth0Client.getIdToken()
+import auth0Client from "./auth/Auth";
+import i18n from "./i18n";
+import { typeDefs, resolvers } from "./queries/resolvers";
+
+// Setup the cache
+const cache = new InMemoryCache({});
+
+// On each request, set the current idToken in the header
+const request = async (operation: any) => {
+  operation.setContext({
+    headers: {
+      authorization: "JWT " + auth0Client.getIdToken()
+    }
+  });
+};
+
+// Here we can chain
+const requestLink = new ApolloLink(
+  (operation, forward) =>
+    new Observable(observer => {
+      let handle: any;
+      Promise.resolve(operation)
+        .then(oper => request(oper))
+        .then(() => {
+          if (!forward) return null;
+          handle = forward(operation).subscribe({
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer)
+          });
+        })
+        .catch(observer.error.bind(observer));
+
+      return () => {
+        if (handle) handle.unsubscribe();
+      };
+    })
+);
+
+const client = new ApolloClient({
+  cache,
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        // TODO: We should send something to sentry...
+        // sendToLoggingService(graphQLErrors);
       }
-    });
+      if (networkError) {
+        // TODO: We should logout the user, if the network error is actually a 400 or something...
+        // handleNetworkError / auth0Client.logout();
+      }
+    }),
+    requestLink,
+    new HttpLink({
+      uri: process.env.REACT_APP_BACKEND_URL
+    })
+  ]),
+  typeDefs: typeDefs,
+  resolvers: resolvers
+});
+
+// These are the default values, if nothing is set in localStorage/the backend
+const defaultSettings = {
+  __typename: "Settings",
+  userName: "",
+  currentRole: (auth0Client && auth0Client.getCurrentRole()) || "viewer",
+  language: i18n.language,
+  translatorLanguages: [],
+  receiveEventNotifications: false,
+  hasCompletedSetup: false
+};
+
+// Get the initial state for settings
+const settings = JSON.parse(
+  localStorage.getItem("settings") || JSON.stringify(defaultSettings)
+);
+
+/**
+ * Write the default/initial values to the cache
+ */
+client.cache.writeData({
+  data: {
+    settings: {
+      ...settings
+    }
   }
 });
+
+export default client;
---
+++ src/pages/Settings/SettingsSection/GeneralSection.tsx
@@ -0,0 +1,72 @@
+import React, { useRef } from "react";
+import { useQuery, useMutation } from "react-apollo-hooks";
+import { useTranslation } from "react-i18next";
+import { Formik, Field, Form } from "formik";
+import { TextField } from "formik-material-ui";
+
+import { withStyles, WithStyles, Grid, MenuItem } from "@material-ui/core";
+
+import { styles } from "src/styles";
+import { GET_SETTINGS, UPDATE_SETTINGS } from "src/queries/settings";
+import auth0Client from "src/auth/Auth";
+
+interface Props extends WithStyles<typeof styles> {
+  settings?: any;
+  setSettings?: CallableFunction;
+  values?: any;
+}
+
+function GeneralSection({ classes, values }: Props) {
+  const { t } = useTranslation();
+  const { data, error, loading } = useQuery(GET_SETTINGS);
+  const updateSettings = useMutation(UPDATE_SETTINGS);
+
+  const { settings } = data;
+  const roles = auth0Client.getAllowedRoles();
+
+  const handleChange = (name: string) => (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    if (name === "currentRole") {
+      auth0Client.changeCurrentRole(event.target.value);
+    } else {
+      updateSettings({
+        variables: { ...settings, [name]: event.target.value }
+      });
+    }
+  };
+
+  return (
+    <Grid item sm={6}>
+      <Field
+        name="userName"
+        label={t("settings:userNameLabel")}
+        component={TextField}
+        margin="normal"
+        fullWidth
+      />
+      {roles.length ? (
+        <Field
+          type="text"
+          name="currentRole"
+          label={t("settings:currentRoleLabel")}
+          select
+          helperText={t("settings:currentRoleHelperText")}
+          margin="normal"
+          component={TextField}
+          fullWidth
+        >
+          {roles.map(r => (
+            <MenuItem key={r} value={r}>
+              {t(r)}
+            </MenuItem>
+          ))}
+        </Field>
+      ) : null}
+    </Grid>
+  );
+}
+
+export default withStyles(styles)(GeneralSection);
