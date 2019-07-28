155114975955973d0f8e47e0c52f10361f8f2705
David Friederich <dav.friede@gmail.com>
2019-07-28 21:28:15 +0200

    additional query fixes for acg

--- src/queries/wordgroups.ts
+++ src/queries/wordgroups.ts
@@ -1,193 +1,150 @@
 import gql from "graphql-tag";
 
 export const GET_WORDGROUPS = gql`
-  query wordGroups {
-  wordGroups {
-    edges {
-      node {
-        id
-        titleCh
-        titleDe
-        fkChapter {
+  query getWordGroups {
+    wordGroups: api_wordgroup {
+      id
+      titleCh: title_ch
+      titleDe: title_de
+      words {
+        word {
           id
-        }
-        words {
-          id
-          wordch {
+          translations {
             id
             audio
             text
-            exampleSentence
-          }
-          wordde {
-            id
-            audio
-            text
-            exampleSentence
-          }
-          worden {
-            id
-            audio
-            text
-            exampleSentence
-          }
-          wordar {
-            id
-            audio
-            text
-            exampleSentence
-          }
-          wordfa {
-            id
-            audio
-            text
-            exampleSentence
+            exampleSentence: example_sentence
+            language {
+              id
+              code
+            }
           }
         }
       }
     }
   }
-}
 `;
 
 export const GET_WORDGROUP_BY_ID = gql`
-  query wordGroupById($id: ID) {
-  wordGroup(id: $id) {
-    id
-    titleCh
-    titleDe
-    words {
+  query getWordGroupById($id: uuid!) {
+    wordGroup: api_wordgroup_by_pk(id: $id) {
       id
-      wordch {
-        id
-        exampleSentence
-        text
-        audio
-      }
-      wordde {
-        id
-        exampleSentence
-        text
-        audio
-      }
-      worden {
-        id
-        exampleSentence
-        text
-        audio
-      }
-      wordar {
-        id
-        exampleSentence
-        text
-        audio
-      }
-      wordfa {
-        id
-        exampleSentence
-        text
-        audio
+      titleCh: title_ch
+      titleDe: title_de
+      words {
+        word {
+          id
+          translations {
+            id
+            audio
+            text
+            exampleSentence: example_sentence
+            language {
+              id
+              code
+            }
+          }
+        }
       }
     }
   }
-}
 `;
 
 export const INSERT_WORDGROUP = gql`
   mutation createWordGroup($input: IntroduceWordGroupInput!) {
-      createWordGroup(input: $input) {
-        wordGroup {
-          id
-          titleCh
-          titleDe
-        }
+    createWordGroup(input: $input) {
+      wordGroup {
+        id
+        titleCh
+        titleDe
       }
     }
+  }
 `;
 
 export const UPDATE_WORDGROUP = gql`
   mutation updateWordGroup($input: UpdateWordGroupInput!) {
-      updateWordGroup(input: $input) {
-        wordGroup {
-          id
-          titleCh
-          titleDe
-        }
+    updateWordGroup(input: $input) {
+      wordGroup {
+        id
+        titleCh
+        titleDe
       }
     }
+  }
 `;
 
 export const CREATE_WORD = gql`
-    mutation createWord($input: IntroduceWordInput!) {
-      createWord(input: $input) {
-        word {
-          id
-        }
+  mutation createWord($input: IntroduceWordInput!) {
+    createWord(input: $input) {
+      word {
+        id
       }
     }
+  }
 `;
 
 export const UPDATE_DE_WORD = gql`
-    mutation updateDEWord($input: UpdateWordDEInput!) {
-      updateDeWord(input: $input) {
-        word {
-          id
-          text
-          exampleSentence
-          audio
-        }
+  mutation updateDEWord($input: UpdateWordDEInput!) {
+    updateDeWord(input: $input) {
+      word {
+        id
+        text
+        exampleSentence
+        audio
       }
     }
+  }
 `;
 
 export const UPDATE_CH_WORD = gql`
-    mutation updateCHWord($input: UpdateWordCHInput!) {
-      updateChWord(input: $input) {
-        word {
-          id
-          text
-          exampleSentence
-          audio
-        }
+  mutation updateCHWord($input: UpdateWordCHInput!) {
+    updateChWord(input: $input) {
+      word {
+        id
+        text
+        exampleSentence
+        audio
       }
     }
+  }
 `;
 
 export const UPDATE_EN_WORD = gql`
-    mutation updateENWord($input: UpdateWordENInput!) {
-      updateEnWord(input: $input) {
-        word {
-          id
-          text
-          exampleSentence
-          audio
-        }
+  mutation updateENWord($input: UpdateWordENInput!) {
+    updateEnWord(input: $input) {
+      word {
+        id
+        text
+        exampleSentence
+        audio
       }
     }
+  }
 `;
 
 export const UPDATE_FA_WORD = gql`
-    mutation updateFAWord($input: UpdateWordFAInput!) {
-      updateFaWord(input: $input) {
-        word {
-          id
-          text
-          exampleSentence
-          audio
-        }
+  mutation updateFAWord($input: UpdateWordFAInput!) {
+    updateFaWord(input: $input) {
+      word {
+        id
+        text
+        exampleSentence
+        audio
       }
     }
+  }
 `;
 
 export const UPDATE_AR_WORD = gql`
-    mutation updateARWord($input: UpdateWordARInput!) {
-      updateArWord(input: $input) {
-        word {
-          id
-          text
-          exampleSentence
-          audio
-        }
+  mutation updateARWord($input: UpdateWordARInput!) {
+    updateArWord(input: $input) {
+      word {
+        id
+        text
+        exampleSentence
+        audio
       }
     }
+  }
 `;
